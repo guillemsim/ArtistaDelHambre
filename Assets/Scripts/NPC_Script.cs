ce2ae2976e6fcd29917a1c3bda0f100b13beaf79
guillemsilvestre <guillemsilvestre@gmail.com>
2026-05-26 14:27:30 +0200

    Npcs

--- Assets/Scripts/NPC_Script.cs
+++ Assets/Scripts/NPC_Script.cs
@@ -1,17 +1,41 @@
+using System.Net.Sockets;
 using UnityEngine;
 
 public class NPC_Script : MonoBehaviour
 {
+    private Vector3 _PosInicial;
+    private float _NPCVelocidad = 5f;
+    private bool _PlayerLejos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _PosInicial = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-    //Vector3 direction = NPCTransform.position - PersonajeTransform.position;
-    //NPC.Move(direction); // this assumes you have custom locomotion
+        if(_PlayerLejos)
+        {
+            float _Mov = _NPCVelocidad * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, _PosInicial, _Mov);
+        }
+        
+    }
+        void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Personaje")
+        {
+            _PlayerLejos = false;
+        }
+
+    }
+
+        void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.name == "Personaje")
+        {
+            _PlayerLejos = true;
+        }
     }
 }
